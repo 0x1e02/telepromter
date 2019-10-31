5473bef4dea1d04fb0bcb1d0a3d4c360a3b93253
Leon Nitsche <leonarnonitsche@gmail.com>
2019-10-31 16:56:07 +0100

    deleted unused variable

--- behavior.js
+++ behavior.js
@@ -1,4 +1,4 @@
-const SPEED = 700;
+const SPEED = 10;
 const PRE_READ = 10;
 const TEXTS = ["Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."]
 
@@ -10,7 +10,6 @@ function telepromter(text){
     var display = document.getElementById("display");
     var intervall = setInterval(function(){
         var slice = text.slice(counter, counter+PRE_READ);
-        var highlight = document.createElement("div");
         display.innerHTML = "<div class='highlight'>" + slice.shift() + "</div> " + slice.join(" ");
         console.log(counter);
         if (++counter == text.length){
