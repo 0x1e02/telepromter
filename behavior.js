f155111eff5080e2f7561892bc4885f50e4edeaf
Leon Nitsche <leonarnonitsche@gmail.com>
2019-11-01 15:41:39 +0100

    variable delay with vocals as defining metric

--- behavior.js
+++ behavior.js
@@ -1,5 +1,4 @@
-const SPEED = 300;
-const PRE_READ = 10;
+const SPEED = 150;
 const TEXTS = ["Die Kritik nach dem CDU-Debakel in Thüringen an Angela Merkel wächst. Zwar hat die Kanzlerin die Führung der Partei vor einem Jahr an Annegreat Kramp-Karrenbauer angegeben. Doch das tatenlose Zusehen der Demontage ihrer Wunschnachfolgerin beunruhigt immer mehr Christdemokraten. Trigema-Chef Wolfgang Grupp erklärt, welche Voraussetzungen für einen Generationenwechsel unabdingbar sind – in der Wirtschaft sowohl als auch in der Politik. Von FOCUS-Online-Redakteur Ulf Lüdeke"];
 
 
@@ -8,13 +7,19 @@ function telepromter(text){
     display = document.getElementById("display");
     display.innerHTML = text.map(x => `<span>${x}</span>`).join(" ");
     let x = 0;
-    let speed = SPEED;
-    var intervall = setInterval(function(){
+    let speed = 0;
+    count_vocals = word => Array.from(word.toLowerCase()).filter(x => "aeiouäöü".includes(x)).length;
+    function timeout(){
         const child = display.children[x];
-        child.className = "highlight";
-        window.scrollTo({left: display.children[x].getBoundingClientRect().x + window.scrollX - 300, behavior: 'smooth'});
-        x++;
-    }, speed);
+        setTimeout(function(){
+            child.className = "highlight";
+            speed = SPEED * (count_vocals(display.children[x].innerHTML) + 1);
+            window.scrollTo({left: display.children[x].getBoundingClientRect().x + window.scrollX - 300, behavior: 'smooth'});
+            x++;
+            timeout();
+        }, speed);
+    }
+    timeout();
 }
 
 document.onclick = function(){
