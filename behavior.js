b7ea153d5e1ec256e281c9fa2905624040cd2132
Leon Nitsche <leonarnonitsche@gmail.com>
2019-11-03 10:22:36 +0100

    unified variable declaration and handeled setting className for undefined array elements

--- behavior.js
+++ behavior.js
@@ -6,29 +6,27 @@ const mono_phonetics = ["au", "ei", "ai", "ey", "ay", "eu", "äu", "ui", "ie", "
 const TEXTS = ["Die Kritik nach dem CDU-Debakel in Thüringen an Angela Merkel wächst. Zwar hat die Kanzlerin die Führung der Partei vor einem Jahr an Annegreat Kramp-Karrenbauer angegeben. Doch das tatenlose Zusehen der Demontage ihrer Wunschnachfolgerin beunruhigt immer mehr Christdemokraten. Trigema-Chef Wolfgang Grupp erklärt, welche Voraussetzungen für einen Generationenwechsel unabdingbar sind – in der Wirtschaft sowohl als auch in der Politik. Von FOCUS-Online-Redakteur Ulf Lüdeke"];
 
 
+function count_vocals(word){
+    word = Array.from(word.toLowerCase());
+    for (let i = 0; i<word.length; i++){
+        let truth_map = mono_phonetics.map( x => word.join("").slice(i, i+x.length) == x);
+        let j = truth_map.indexOf(true);
+        if (j != -1){
+            word[i] += word.splice(i+1, mono_phonetics[j].length-1);
+        }
+    }
+    /* don't touch it works.. thats good enough */
+    return word.filter(x=>Array.from(x)
+    .map(y => ! "aeiouäöü".includes(y))
+    .reduce((a,b) => a+b) == 0).length;
+}
+
 function telepromter(text){
-    text = text.split(" ");
-    display = document.getElementById("display");
-    display.innerHTML = text.map(x => `<span>${x}</span>`).join(" ");
+    var text = text.split(" ");
+    let display = document.getElementById("display");
     let x = 0;
     let speed = 0;
-    // count_vocals = word => Array.from(word.toLowerCase()).filter(x => "aeiouäöü".includes(x)).length;
-    function count_vocals(word){
-        word = Array.from(word.toLowerCase());
-        for (let i = 0; i<word.length; i++){
-            var truth_map = mono_phonetics.map( x => word.join("").slice(i, i+x.length) == x);
-            var j = truth_map.indexOf(true);
-            if (j != -1){
-                word[i] += word.splice(i+1, mono_phonetics[j].length-1);
-            }
-        }
-        /* don't touch it works.. thats good enough */
-        return word.filter(x=>Array.from(x)
-        .map(y => ! "aeiouäöü".includes(y))
-        .reduce((a,b) => a+b) == 0).length;
-    }
-
-    text.forEach( x => console.log(count_vocals(x)));
+    display.innerHTML = text.map(x => `<span>${x}</span>`).join(" ");
 
     function timeout(){
         const child = display.children[x];
@@ -36,8 +34,10 @@ function telepromter(text){
             child.className = "highlight";
             speed = SPEED * (count_vocals(display.children[x].innerHTML));
             window.scrollTo({left: display.children[x].getBoundingClientRect().x + window.scrollX - 300, behavior: 'smooth'});
-            x++;
-            timeout();
+            if (++x < text.length){
+                timeout();
+            }
+            
         }, speed);
     }
     timeout();
